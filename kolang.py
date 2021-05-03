7b85f668187a2282f80701d068922178691bf786
kangsanha <68986305+sanha9999@users.noreply.github.com>
2021-05-03 19:50:02 +0900

    matplotlib에서 title같은 것을 한국어로 하고 싶을때 import해서 사용

---
+++ kolang.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+
+def kolang(size):
+    font_path = r'D:/font/NanumBarunpenB.ttf'
+    fontprop = fm.FontProperties(fname=font_path, size=size)
+
+    return fontprop
